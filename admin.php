2d31dedf4102751c4bda0e31b5f33449376ba245
TheJorkits <113550647+TheJorkits@users.noreply.github.com>
2022-09-14 15:37:53 -0500

    Add files via upload

---
+++ admin.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body BGCOLOR="#F74F4F">
+<a HREF="inicio.php" >volver</a>
+<p align="center"><IMG SRC="ventas1.jpg"></P>
+</body>
+</html>
